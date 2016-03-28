a2e694899574074f107b968c93f2fd7d4deb67db
Joechen1210 <joes9630003@gmail.com>
2016-03-28 13:59:40 +0800

    Update index.js

--- canvas_test/js/index.js
+++ canvas_test/js/index.js
@@ -21,7 +21,7 @@ function playVideo(){
 	video.addEventListener('canplaythrough',ready);
 	audio.addEventListener('canplaythrough',ready);
 	
-	audio.src = video.src = "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_10mb.mp4";
+	audio.src = video.src = "http://brightcove.vo.llnwd.net/e1/uds/pd/96980657001/96980657001_109379494001_Bird-CommonRedpoll-iStock-000006369683HD720.mp4?pubId=4005328949001&videoId=4006182806001";
 	
 	audio.load();
 	video.load();
