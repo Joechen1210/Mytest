7a4442b3d0d61f50be6794f12fa4a8cb4e5ea0cf
Joechen1210 <joes9630003@gmail.com>
2016-03-28 17:15:10 +0800

    Update index.js

--- canvas_test/js/index.js
+++ canvas_test/js/index.js
@@ -1,14 +1,25 @@
+
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext('2d');
+var myImage = new Image();
+myImage.src = "http://www.conceptcarz.com/images/Citroen/2010-Citroen-Survolt-Concept-Image-01.jpg";
+myImage.onload = function() 
+{
+  ctx.drawImage(myImage, 0, 0, canvas.width,canvas.height);	
+};
 var audio = document.createElement('audio');
 var video = document.createElement('video');
-
 function onFrame(){
-	ctx.drawImage(video,0,0,426,240);
+	try{
+	ctx.drawImage(video,0,0,canvas.width,canvas.height);
 	video.currentTime = audio.currentTime;
 	requestAnimationFrame(onFrame);
+	}
+	catch (err)
+	{
+	 return false;
+	}
 }
-
 function playVideo(){
 	var i = 0;
 	function ready(){
@@ -20,9 +31,7 @@ function playVideo(){
 	}
 	video.addEventListener('canplaythrough',ready);
 	audio.addEventListener('canplaythrough',ready);
-	
 	audio.src = video.src = "http://brightcove.vo.llnwd.net/e1/uds/pd/96980657001/96980657001_109379494001_Bird-CommonRedpoll-iStock-000006369683HD720.mp4?pubId=4005328949001&videoId=4006182806001";
-	
 	audio.load();
 	video.load();
 }
